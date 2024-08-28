0cb876e91829c5b435f491eff9aa88e3b3d71e81
p3javier <javii.e.saverroes@gmail.com>
2024-08-28 15:18:45 +0200

    feat: added linkedin insight tag tracking code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -34,6 +34,30 @@ export default function RootLayout({
         <NavBar />
         <div className="flex-grow min-w-full">{children}</div>
         <Footer />
+        <Script id="__linkedin_partner_id__">
+          {`_linkedin_partner_id = "7569273";
+            window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+            window._linkedin_data_partner_ids.push(_linkedin_partner_id);`}
+        </Script>
+        <Script id="__linkedin_tracking__">
+          {`(function(l) {
+            if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+            window.lintrk.q=[]}
+            var s = document.getElementsByTagName("script")[0];
+            var b = document.createElement("script");
+            b.type = "text/javascript";b.async = true;
+            b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+            s.parentNode.insertBefore(b, s);})(window.lintrk);`}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            className="hidden"
+            alt=""
+            src="https://px.ads.linkedin.com/collect/?pid=7569273&fmt=gif"
+          />
+        </noscript>
       </body>
     </html>
   );
