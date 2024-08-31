01269dcd49af13b6522d2b96f2f553312637b994
p3javier <javii.e.saverroes@gmail.com>
2024-08-31 11:20:25 +0200

    feat: added id to script

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -23,7 +23,7 @@ export const Footer: React.FC = () => {
       >
         Política de Privacidad
       </a>
-      <Script>
+      <Script id="_privacy_policy_button_">
         {`(function (w,d) {var loader = function () {var s = d.createElement("script"), tag = d.getElementsByTagName("script")[0]; s.src="https://cdn.iubenda.com/iubenda.js"; tag.parentNode.insertBefore(s,tag);}; if(w.addEventListener){w.addEventListener("load", loader, false);}else if(w.attachEvent){w.attachEvent("onload", loader);}else{w.onload = loader;}})(window, document);`}
       </Script>
       <Script id="__linkedin_partner_id__">
